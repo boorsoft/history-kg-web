875f8fce6eebb8236525bf9bd31e4ca8eabfff6d
akmatoff <akmatoff.exe@gmail.com>
2022-05-26 19:31:22 +0600

    UPDATE: add token to the admin requests

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,24 +1,49 @@
 import React from "react";
-import { Admin, Resource } from 'react-admin'
-import restProvider from 'ra-data-simple-rest'
+import { Admin, fetchUtils, Options, Resource } from "react-admin";
+import restProvider from "ra-data-simple-rest";
 
 import { API_URL } from "../../constants/constants";
 
-import ParagraphsList from './components/Paragraph/ParagraphsList';
-import ParagraphCreate from './components/Paragraph/ParagraphCreate';
-import ParagraphEdit from './components/Paragraph/ParagraphEdit';
+import ParagraphsList from "./components/Paragraph/ParagraphsList";
+import ParagraphCreate from "./components/Paragraph/ParagraphCreate";
+import ParagraphEdit from "./components/Paragraph/ParagraphEdit";
 import PersonsList from "./components/Person/PersonsList";
 import PersonCreate from "./components/Person/PersonCreate";
 import PersonEdit from "./components/Person/PersonEdit";
 import authProvider from "./authProvider";
 
+const httpClient = (url: string, options: Options = {}) => {
+  if (!options.headers) {
+    options.headers = new Headers({
+      Accept: "application/json",
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    });
+  }
+  return fetchUtils.fetchJson(url, options);
+};
+
 const AdminPage = () => {
-    return (
-        <Admin basename="/admin" authProvider={authProvider} dataProvider={restProvider(API_URL)} requireAuth>
-            <Resource name="paragraphs" list={ParagraphsList} create={ParagraphCreate} edit={ParagraphEdit} />
-            <Resource name="persons" list={PersonsList} create={PersonCreate} edit={PersonEdit} />
-        </Admin>
-    )
-}
+  return (
+    <Admin
+      basename="/admin"
+      authProvider={authProvider}
+      dataProvider={restProvider(API_URL, httpClient)}
+      requireAuth
+    >
+      <Resource
+        name="paragraphs"
+        list={ParagraphsList}
+        create={ParagraphCreate}
+        edit={ParagraphEdit}
+      />
+      <Resource
+        name="persons"
+        list={PersonsList}
+        create={PersonCreate}
+        edit={PersonEdit}
+      />
+    </Admin>
+  );
+};
 
 export default AdminPage;
--- src/pages/Admin/components/Paragraph/ParagraphCreate.tsx
+++ src/pages/Admin/components/Paragraph/ParagraphCreate.tsx
@@ -5,7 +5,7 @@ import { RichTextInput } from 'ra-input-rich-text';
 
 const ParagraphCreate: FC = (props) => {
     return (
-        <Create  title='Create a paragraph' {...props}>
+        <Create title='Create a paragraph' {...props}>
             <SimpleForm>
                 <TextInput source="title" />
                 <RichTextInput source="text" />
