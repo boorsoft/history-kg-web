75e61a134e1e32c86e0deda75ca21a370cdf25c6
akmatoff <akmatoff.exe@gmail.com>
2022-05-30 19:50:36 +0600

    FIX: Quiz Edit fix

--- src/pages/Admin/components/Quiz/QuizEdit.tsx
+++ src/pages/Admin/components/Quiz/QuizEdit.tsx
@@ -1,29 +1,36 @@
 import React from "react";
 import { FC } from "react";
-import { ArrayInput, BooleanInput, Create, SimpleForm, SimpleFormIterator, TextInput } from "react-admin";
+import {
+  ArrayInput,
+  BooleanInput,
+  Edit,
+  SimpleForm,
+  SimpleFormIterator,
+  TextInput,
+} from "react-admin";
 
 const QuizEdit: FC = (props) => {
-    return (
-        <Create title="Create a quiz" {...props}>
-            <SimpleForm>
+  return (
+    <Edit title="Create a quiz" {...props}>
+      <SimpleForm>
+        <TextInput source="id" disabled />
+        <TextInput source="title" />
+        <ArrayInput source="questions">
+          <SimpleFormIterator>
+            <TextInput source="id" disabled />
+            <TextInput source="text" />
+            <ArrayInput source="answers">
+              <SimpleFormIterator>
                 <TextInput source="id" disabled />
-                <TextInput source="title" />
-                <ArrayInput source="questions">
-                    <SimpleFormIterator>
-                        <TextInput source="id" disabled />
-                        <TextInput source="text" />
-                        <ArrayInput source="answers">
-                            <SimpleFormIterator>
-                                <TextInput source="id" disabled />
-                                <TextInput source="text" />
-                                <BooleanInput source="isCorrectAnswer" />
-                            </SimpleFormIterator>
-                        </ArrayInput>
-                    </SimpleFormIterator>
-                </ArrayInput>
-            </SimpleForm>
-        </Create>
-    )
-}
+                <TextInput source="text" />
+                <BooleanInput source="isCorrectAnswer" />
+              </SimpleFormIterator>
+            </ArrayInput>
+          </SimpleFormIterator>
+        </ArrayInput>
+      </SimpleForm>
+    </Edit>
+  );
+};
 
 export default QuizEdit;
