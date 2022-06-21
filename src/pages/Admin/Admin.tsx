8d078d54e06b7520ef4544bf5783aebc5e0cfc67
akmatoff <akmatoff.exe@gmail.com>
2022-06-21 16:59:33 +0600

    FIX: Delete records from admin page

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,6 +1,13 @@
 import React from "react";
-import { Admin, fetchUtils, Options, Resource } from "react-admin";
+import {
+    Admin,
+    DataProvider,
+    fetchUtils,
+    Options,
+    Resource,
+} from "react-admin";
 import restProvider from "ra-data-simple-rest";
+import axios from "axios";
 
 import { API_URL } from "../../constants/constants";
 
@@ -14,46 +21,61 @@ import authProvider from "./authProvider";
 import QuizList from "./components/Quiz/QuizList";
 import QuizCreate from "./components/Quiz/QuizCreate";
 import QuizEdit from "./components/Quiz/QuizEdit";
+import { useParams } from "react-router-dom";
 
 const httpClient = (url: string, options: Options = {}) => {
-  if (!options.headers) {
-    options.headers = new Headers({
-      Accept: "application/json",
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-    });
-  }
+    if (!options.headers) {
+        options.headers = new Headers({
+            Accept: "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        });
+    }
 
-  return fetchUtils.fetchJson(url, options);
+    return fetchUtils.fetchJson(url, options);
 };
 
 const AdminPage = () => {
-  return (
-    <Admin
-      basename="/admin"
-      authProvider={authProvider}
-      dataProvider={restProvider(API_URL, httpClient)}
-      requireAuth
-    >
-      <Resource
-        name="paragraphs"
-        list={ParagraphsList}
-        create={ParagraphCreate}
-        edit={ParagraphEdit}
-      />
-      <Resource
-        name="persons"
-        list={PersonsList}
-        create={PersonCreate}
-        edit={PersonEdit}
-      />
-      <Resource 
-        name="quiz"
-        list={QuizList}
-        create={QuizCreate}
-        edit={QuizEdit}      
-      />
-    </Admin>
-  );
+    const pageParams = useParams();
+
+    const dataProvider: DataProvider = {
+        ...restProvider(API_URL, httpClient),
+        delete: (resource, params) =>
+            axios({
+                method: "DELETE",
+                url: `${API_URL}/${pageParams['*']}/${params.id}`,
+                headers: {
+                  Authorization: `Bearer ${localStorage.getItem("token")}`,
+                }
+            }),
+    };
+
+    return (
+        <Admin
+            basename="/admin"
+            authProvider={authProvider}
+            dataProvider={dataProvider}
+            requireAuth
+        >
+            <Resource
+                name="paragraphs"
+                list={ParagraphsList}
+                create={ParagraphCreate}
+                edit={ParagraphEdit}
+            />
+            <Resource
+                name="persons"
+                list={PersonsList}
+                create={PersonCreate}
+                edit={PersonEdit}
+            />
+            <Resource
+                name="quiz"
+                list={QuizList}
+                create={QuizCreate}
+                edit={QuizEdit}
+            />
+        </Admin>
+    );
 };
 
 export default AdminPage;
