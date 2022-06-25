a30fe41cce584de9a7a725f084a8ffeeb9aca9ce
akmatoff <akmatoff.exe@gmail.com>
2022-06-26 01:47:23 +0600

    allow base64 in rich text editor

--- src/components/CustomRichTextInput.tsx
+++ src/components/CustomRichTextInput.tsx
@@ -1,56 +1,60 @@
 import React, { useCallback } from "react";
 import { FC } from "react";
 import {
-  DefaultEditorOptions,
-  RichTextInput,
-  RichTextInputToolbar,
-  LevelSelect,
-  FormatButtons,
-  AlignmentButtons,
-  ListButtons,
-  LinkButtons,
-  QuoteButtons,
-  ClearButtons,
-  useTiptapEditor,
-  RichTextInputProps
+    DefaultEditorOptions,
+    RichTextInput,
+    RichTextInputToolbar,
+    LevelSelect,
+    FormatButtons,
+    AlignmentButtons,
+    ListButtons,
+    LinkButtons,
+    QuoteButtons,
+    ClearButtons,
+    useTiptapEditor,
+    RichTextInputProps,
 } from "ra-input-rich-text";
 import Image from "@tiptap/extension-image";
 
-const CustomRichTextInput: FC<RichTextInputProps> = ({...props}: RichTextInputProps) => {
+const CustomRichTextInput: FC<RichTextInputProps> = ({
+    ...props
+}: RichTextInputProps) => {
     const editor = useTiptapEditor();
 
     const addImage = useCallback(() => {
         if (!editor) return;
 
-        const url = window.prompt('URL')
-      
+        const url = window.prompt("URL");
+
         if (url) {
-          editor && editor.chain().focus().setImage({src: url}).run();  
+            editor && editor.chain().focus().setImage({ src: url }).run();
         }
+    }, []);
 
-    }, [])
-
-  return (
-    <RichTextInput
-      editorOptions={{
-        ...DefaultEditorOptions,
-        extensions: [...DefaultEditorOptions.extensions, Image],
-      }}
-      toolbar={
-        <RichTextInputToolbar>
-          <LevelSelect />
-          <FormatButtons />
-          <AlignmentButtons />
-          <ListButtons />
-          <LinkButtons />
-          <QuoteButtons />
-          <ClearButtons />
-          <div onClick={addImage}>Image</div>
-        </RichTextInputToolbar>
-      }
-      {...props}
-    />
-  );
+    return (
+        <RichTextInput
+            editorOptions={{
+                ...DefaultEditorOptions,
+                extensions: [
+                    ...DefaultEditorOptions.extensions,
+                    Image.configure({ allowBase64: true }),
+                ],
+            }}
+            toolbar={
+                <RichTextInputToolbar>
+                    <LevelSelect />
+                    <FormatButtons />
+                    <AlignmentButtons />
+                    <ListButtons />
+                    <LinkButtons />
+                    <QuoteButtons />
+                    <ClearButtons />
+                    <div onClick={addImage}>Image</div>
+                </RichTextInputToolbar>
+            }
+            {...props}
+        />
+    );
 };
 
 export default CustomRichTextInput;
