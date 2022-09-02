181313a94a4d924acfb65bcdf753c04c8fddc529
akmatoff <akmatoff.exe@gmail.com>
2022-09-02 23:07:39 +0600

    [FEAT] Customize toolbar

--- src/pages/PDFViewer/PDFViewer.tsx
+++ src/pages/PDFViewer/PDFViewer.tsx
@@ -1,14 +1,18 @@
-import React, { useEffect } from "react";
+import React, { ReactElement, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Worker, Viewer } from "@react-pdf-viewer/core";
 import { zoomPlugin } from "@react-pdf-viewer/zoom";
-import { defaultLayoutPlugin, ToolbarProps } from '@react-pdf-viewer/default-layout';
+import {
+  defaultLayoutPlugin,
+  ToolbarProps,
+  ToolbarSlot,
+} from "@react-pdf-viewer/default-layout";
 
 import styled from "styled-components";
 
 import "@react-pdf-viewer/core/lib/styles/index.css";
-import '@react-pdf-viewer/zoom/lib/styles/index.css';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import "@react-pdf-viewer/zoom/lib/styles/index.css";
+import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
 import Header from "../../components/Header";
 import { AppDispatch, RootState } from "../../store/store";
@@ -28,9 +32,71 @@ const PDFViewer = () => {
   const book = currentBook;
 
   const getBook = bindActionCreators(fetchBookById, dispatch);
-  
-  const zoomPluginInstance = zoomPlugin({enableShortcuts: true});
-  const defaultLayoutPluginInstance = defaultLayoutPlugin({sidebarTabs: () => []});
+  const renderToolbar = (Toolbar: (props: ToolbarProps) => ReactElement) => (
+    <Toolbar>
+      {(slots: ToolbarSlot) => {
+        const {
+          CurrentPageInput,
+          Download,
+          EnterFullScreen,
+          GoToNextPage,
+          GoToPreviousPage,
+          NumberOfPages,
+          Print,
+          ShowSearchPopover,
+          Zoom,
+          ZoomIn,
+          ZoomOut,
+        } = slots;
+        return (
+          <div
+            style={{
+              alignItems: "center",
+              display: "flex",
+              width: "100%",
+            }}
+          >
+            <div style={{ padding: "0px 2px" }}>
+              <ShowSearchPopover />
+            </div>
+            <div style={{ padding: "0px 2px" }}>
+              <ZoomOut />
+            </div>
+            <div style={{ padding: "0px 2px" }}>
+              <Zoom />
+            </div>
+            <div style={{ padding: "0px 2px" }}>
+              <ZoomIn />
+            </div>
+            <div style={{ padding: "0px 2px", marginLeft: "auto" }}>
+              <GoToPreviousPage />
+            </div>
+            <div style={{ padding: "0px 2px", flexDirection: "row", display: "flex", alignItems: "center" }}>
+              <CurrentPageInput /> / <NumberOfPages />
+            </div>
+            <div style={{ padding: "0px 2px" }}>
+              <GoToNextPage />
+            </div>
+            <div style={{ padding: "0px 2px", marginLeft: "auto" }}>
+              <EnterFullScreen />
+            </div>
+            {/* <div style={{ padding: "0px 2px" }}>
+              <Download />
+            </div>
+            <div style={{ padding: "0px 2px" }}>
+              <Print />
+            </div> */}
+          </div>
+        );
+      }}
+    </Toolbar>
+  );
+
+  const zoomPluginInstance = zoomPlugin({ enableShortcuts: true });
+  const defaultLayoutPluginInstance = defaultLayoutPlugin({
+    sidebarTabs: () => [],
+    renderToolbar
+  });
 
   useEffect(() => {
     id && getBook(+id);
@@ -43,7 +109,11 @@ const PDFViewer = () => {
       <Container>
         <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.15.349/build/pdf.worker.min.js">
           <PDFWrapper>
-            <Viewer plugins={[zoomPluginInstance, defaultLayoutPluginInstance]} defaultScale={1.0} fileUrl={`${BASE_URL}/books/${book?.fileName}`} />
+            <Viewer
+              plugins={[zoomPluginInstance, defaultLayoutPluginInstance]}
+              defaultScale={1.0}
+              fileUrl={`${BASE_URL}/books/${book?.fileName}`}
+            />
           </PDFWrapper>
         </Worker>
       </Container>
